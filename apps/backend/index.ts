d5753728818525af75cb02364263a8887b6fdab4
Hp <rs739025@gmail.com>
2026-06-28 10:12:46 +0530

    fix the index.ts

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -1,3 +1,4 @@
+import 'dotenv/config' 
 import express from "express";
 import authRoute from "./src/features/auth/auth_routes";
 import cookieParser from "cookie-parser";
@@ -5,6 +6,7 @@ import { globalErrorHandler } from "./src/middleware/error.middleware";
 import { authentication } from "./src/middleware/auth.middleware";
 import avatarRoute from "./src/features/avatar/avatar_routes";
 import videoRoute from "./src/features/video/video_routes";
+import './src/features/video/video_worker';
 
 const app = express();
 const PORT = process.env.PORT
@@ -12,15 +14,16 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use(cookieParser())
 
-app.use('api/auth',authRoute)
+app.use('/api/auth',authRoute)
 
 //protected route 
 
-app.use('api/avatars', authentication, avatarRoute)
-app.use('api/videos', authentication, videoRoute)
+app.use('/api/avatars', authentication, avatarRoute)
+app.use('/api/videos', videoRoute)
 
 
 app.use(globalErrorHandler)
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`)
+    console.log(`Server is running at http://localhost:${PORT}`),
+    console.log(`[worker] video generation worker started`)
 })
